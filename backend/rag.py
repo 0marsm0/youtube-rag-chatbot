1045eea173ed119d04a9a320853413e8165ac0c3
0marsm0 <alish.yuldashev88@gmail.com>
2025-12-14 11:50:38 +0100

    add requirements, change structure and logic in rag & function_app

--- backend/rag.py
+++ backend/rag.py
@@ -5,9 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+vector_db = lancedb.connect(uri=VECTOR_DATABASE_PATH)
+
 youtuber_agent = Agent(
     model="google-gla:gemini-2.5-flash",
-    retries=2,
     system_prompt="The Youtuber is knowledgeable in the data engineering field with a nerdy humour. He is making Tutorial videos. Your target audience is students trying to learn Data Engineering. Try to make your answeres similar to The Youtuber's personality.",
 )
 
@@ -17,18 +18,14 @@ def retrieve_context(query: str) -> str:
     """
     Retrieve relevant context from the vector database based on the user's query.
     """
-    vector_db = lancedb.connect(uri=VECTOR_DATABASE_PATH)
-    table = vector_db.open_table("transcripts")
-    results = table.search(query=query).limit(3).to_list()
+    try:
+        table = vector_db.open_table("transcripts")
+        results = table.search(query=query).limit(3).to_list()
 
-    result_string = ""
-    for item in results:
-        result_string += f"Title: {item['title']}\nText: {item['text']}\n\n"
+        result_string = ""
+        for item in results:
+            result_string += f"Title: {item['title']}\nText: {item['text']}\n\n"
 
-    return result_string
-
-
-if __name__ == "__main__":
-    response = youtuber_agent.run_sync("What is lancedb?")
-
-    print(response.output)
+        return result_string
+    except Exception as e:
+        return f"Error connecting to DB: {e}"
--- function_app.py
+++ function_app.py
@@ -1,41 +1,11 @@
 import azure.functions as func
-import logging
-import json
-import nest_asyncio
-from backend.rag import youtuber_agent
-
-nest_asyncio.apply()
+import api
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
-@app.route(route="chat")
-async def chat(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Processing chat request.")
-
-    try:
-        req_body = req.get_json()
-        user_prompt = req_body.get("prompt")
-
-        if not user_prompt:
-            return func.HttpResponse(
-                json.dumps({"error": "No prompt provided"}),
-                status_code=400,
-                mimetype="application/json",
-            )
-
-        result = await youtuber_agent.run(user_prompt)
-
-        answer_text = result.output
-
-        return func.HttpResponse(
-            json.dumps({"response": answer_text}),
-            status_code=200,
-            mimetype="application/json",
-        )
-
-    except Exception as e:
-        logging.error(f"CRITICAL ERROR: {str(e)}")
-        return func.HttpResponse(
-            json.dumps({"error": str(e)}), status_code=500, mimetype="application/json"
-        )
+@app.route(route="{*route}", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+async def fastapi_proxy(
+    req: func.HttpRequest, context: func.Context
+) -> func.HttpResponse:
+    return await func.AsgiMiddleware(api.app).handle_async(req, context)
